53cdfcf6ca289f41922993fa339df944492afd18
Jan Singon Slany <slanyj@centrum.cz>
2018-12-28 22:04:19 +0100

    Add documentation

--- src/ArrayDataEntry.php
+++ src/ArrayDataEntry.php
@@ -2,13 +2,13 @@
 namespace Haluz;
 
 class ArrayDataEntry implements DataEntry {
-	
+
 	private $data;
-	
+
 	public function __construct(array $data = array()) {
 		$this->data = $data;
 	}
-	
+
 	public function asArray() {
 		return $this->data;
 	}
--- src/DataEntry.php
+++ src/DataEntry.php
@@ -1,7 +1,18 @@
 <?php
 namespace Haluz;
 
+/**
+ * A single set of data to be used when rendering a template in one instance
+ * of rendering the templates.
+ *
+ * @author Singon
+ */
 interface DataEntry {
+	
+	/**
+	 * Returns the data for the template as an associative array where each
+	 * variable name is mapped to its value.
+	 */
 	public function asArray();
 }
 ?>
