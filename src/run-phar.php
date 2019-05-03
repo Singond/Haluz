fccefe8e7f34a69741138c506b9befae360e80c2
Jan Singon Slany <slanyj@centrum.cz>
2019-05-03 21:50:46 +0200

    Hard-code version into archive on build

--- src/run-phar.php
+++ src/run-phar.php
@@ -5,6 +5,8 @@ namespace Haluz;
 require_once \Phar::running() . DIRECTORY_SEPARATOR . 'vendor'
 	. DIRECTORY_SEPARATOR . 'autoload.php';
 
-$app = new Application();
+$version = 'VAR_VERSION';
+
+$app = new Application($version);
 $app->run();
 ?>
